e44606cd79a0859219ef9e156823bb8946294f95
ConnectPrakash <pp3662504@gmail.com>
2025-04-08 20:55:02 +0530

    first commit

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,10 @@
+// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'path'
 
-// For TS projects, you can also add vite-tsconfig-paths
-// import tsconfigPaths from 'vite-tsconfig-paths'
-
 export default defineConfig({
-  plugins: [
-    react(),
-    // tsconfigPaths() // uncomment if using vite-tsconfig-paths
-  ],
+  plugins: [react()],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, 'src'),
