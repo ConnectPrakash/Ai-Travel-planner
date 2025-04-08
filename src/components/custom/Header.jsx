cf46e92fd67bd18f06a7ed5a4cfdc4cb8ae40e9e
ConnectPrakash <pp3662504@gmail.com>
2025-04-08 20:37:04 +0530

    first commit

--- src/components/custom/Header.jsx
+++ src/components/custom/Header.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import logo from '/logo.svg' // from public folder
 import { Button } from "@/components/ui/button"
+
 function Header() {
   return (
-    <div  className='p-3 shadow-sm flex justify-between items-center px-5'>
-      <img src='/logo.svg'/>
+    <div className="p-3 shadow-sm flex justify-between items-center px-5">
+      <img src={logo} alt="Logo" className="h-10" />
       <div>
         <Button>Sign In</Button>
       </div>
