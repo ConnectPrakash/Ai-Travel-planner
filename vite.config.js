fa2f19131263d41327549fc4b9cfe30b1074aca4
ConnectPrakash <pp3662504@gmail.com>
2025-04-08 22:07:46 +0530

    first commit

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,13 @@
-// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import path from 'path'
+import tailwindcss from '@tailwindcss/postcss'
+import autoprefixer from 'autoprefixer'
 
 export default defineConfig({
   plugins: [react()],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, 'src'),
+  css: {
+    postcss: {
+      plugins: [tailwindcss, autoprefixer],
     },
   },
 })
